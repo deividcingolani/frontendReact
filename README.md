f73da310a3497aa2a82edcce007e512c27faac46
David Cingolani <cingolanidavid@gmail.com>
2020-12-24 08:27:56 -0300

    add script for fill db

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 This is a repo for a Test.
 
+Firt you need install and run backend
+https://github.com/deividcingolani/backendMovies
+
+
 Steps for start application
 1) yarn install
 2) yan start
-3) Go to page http://localhost:3020
+3) complete api with data dummy 
+   node fillDB.mjs
+
+4) Go to page http://localhost:3020
+
+
---
+++ fillDB.mjs
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+for (let i = 0; i < 20; i++) {
+    const body ={
+        "title": "My Movie"+1,
+        "description": "This is my Movie 1",
+        "duration": i*10,
+        "yearOfPublished": 2020
+    }
+    axios
+        .post("http://0.0.0.0:8000/api/movies/", {
+            credentials: "omit",
+            headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
+                Accept: "application/json, text/plain, */*",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            referrer: "http://localhost:3021/",
+            body:JSON.stringify(body),
+            method: "POST",
+            mode: "cors",
+        })
+        .then((res) => console.log(res.data))
+        .catch((e) => console.log(e));
+
+}
